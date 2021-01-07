a007a6da71392282eeaf146de2a48d6e44e08914
murilo <murilovidal@gmail.com>
2021-01-07 13:19:04 -0300

    Minimum functionality bot

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
-require('./token.js');
+require('dotenv').config();
 
 const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf(token)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Bem vindo!'))
 bot.help((ctx) => ctx.reply('Use /nasa para receber a imagem do dia.'))
 bot.command('nasa', (ctx) => ctx.reply('https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/images/672309main_M107_full.jpg'))
 bot.hears('oi', (ctx) => ctx.reply('Olá.'))
 bot.launch()
 
-
+//Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
