2e12562127421312050898cc5e356913c9134210
murilo <murilovidal@gmail.com>
2021-01-12 19:57:27 -0300

    Error handling

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const { Telegraf } = require('telegraf')
 const axios = require('axios');
-
-const URL = 'https://api.nasa.gov/planetary/apod?api_key=' + process.env.API_KEY;
-const URL_RANDOM = 'https://api.nasa.gov/planetary/apod?api_key=' + process.env.API_KEY + '&count=1';
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const URL = process.env.URL_API + process.env.API_KEY;
+const URL_RANDOM = process.env.URL_API + process.env.API_KEY + '&count=1';
+
+const ONE_HOUR = 3600*10^3;
 
 //APOD - Astronomy picture of the day
 async function getJSONAPOD(url){
@@ -12,44 +14,122 @@ async function getJSONAPOD(url){
     return axios.get(url);
   } catch (e) {
     console.log(e);
+    throw 'Failed to retrieve JSON from ' + url;
   }
 };
 
+async function sendImageURL(ctx, json) {
+  if (json.data[0]) {
+    await ctx.replyWithPhoto({url:json.data[0].url}, {caption: json.data[0].title});
+    console.log("Random PhotoURL sent.");
+  }else {
+    await ctx.replyWithPhoto({url:json.data.url}, {caption: json.data.title});
+    console.log("PhotoURL sent.");
+  }
+};
+//Lançar erro nas funções send e tratar na main
+async function sendVideoURL(ctx, json) {
+  if (Array.isArray(json)) {
+    try {
+      await ctx.reply({url:json.data[0].url}, {caption: json.data[0].title});
+      await ctx.reply(json.data[0].explanation);
+      console.log("Random VideoURL sent.");
+    } catch (e) {
+      await ctx.reply('Falha ao recuperar a imagem do dia. :(');
+      console.log("Falhou.");
+      console.log(e);
+    }
+    await ctx.reply({url:json.data[0].url}, {caption: json.data[0].title});
+    await ctx.reply(json.data[0].explanation);
+    console.log("Random VideoURL sent.");
+  }else {
+    try {
+      await ctx.reply({url:json.data.url}, {caption: json.data.title});
+      await ctx.reply(json.data.explanation);
+      console.log("VideoURL sent.");
+    } catch (e) {
+      await ctx.reply('Falha ao recuperar a imagem do dia. :(');
+      console.log("Falhou.");
+      console.log(e);
+    }
+  }
+};
+//Lançar erro nas funções send e tratar na main
+async function sendImageExplanation(ctx, json) {
+  if (json.data[0]) {
+    try {
+      await ctx.reply(json.data[0].explanation);
+      console.log("Random explanation sent.");
+    } catch (e) {
+      await ctx.reply('Falha ao recuperar a imagem do dia. :(');
+      console.log("Falhou.");
+      console.log(e);
+    }
+  } else {
+    try {
+      await ctx.reply(json.data.explanation);
+      console.log("Explanation sent.");
+    } catch (e) {
+      await ctx.reply('Falha ao recuperar a imagem do dia. :(');
+      console.log("Falhou.");
+      console.log(e);
+    }
+  }
+};
 
 (async () => {
-    let jsonNASA = await getJSONAPOD(URL);
+
+    let jsonNASA;
+    try {
+      jsonNASA = await getJSONAPOD(URL);
+    } catch (e) {
+      jsonNASA = null;
+    };
     //Atualizar JSON a cada hora
-    setInterval(async function(){ jsonNASA = await getJSONAPOD(URL); }, 3600*10^3);
+    setInterval(async function(){
+      try {
+        jsonNASA = await getJSONAPOD(URL);
+      } catch (e) {
+        jsonNASA = null;;
+    };}, ONE_HOUR);
 
     bot.start((ctx) => ctx.reply('Bem vindo!'));
     bot.help((ctx) => ctx.reply('Use /imagem para receber a imagem do dia ou /random para receber uma imagem aleatória.'));
-    bot.command('random',async (ctx)=> {
-      let jsonRandom = await getJSONAPOD(URL_RANDOM);
-      if (jsonRandom.data[0].media_type == 'image') {
-        await ctx.replyWithPhoto({url:jsonRandom.data[0].hdurl}, {caption: jsonRandom.data[0].title});
-        ctx.reply(jsonRandom.data[0].explanation);
-      }
-      if (jsonRandom.data[0].media_type == 'video') {
-        ctx.reply({url:jsonRandom.data[0].url}, {caption: jsonRandom.data[0].title});
-        ctx.reply(jsonRandom.data[0].explanation);
+
+    bot.command('imagem', async (ctx) => {
+      if (jsonNASA) {
+        if (jsonNASA.data.media_type == 'image') {
+          await sendImageURL(ctx, jsonNASA);
+          await sendImageExplanation(ctx, jsonNASA);
+        }else if (jsonNASA.data.media_type == 'video') {
+          await sendVideoURL(ctx, jsonNASA);
+        }
+      }else{
+        await ctx.reply('Falha ao recuperar a imagem do dia. :(');
       }
     });
-    if (jsonNASA.data.media_type == 'image') {
-      bot.command('imagem', (ctx) => {
-        await ctx.replyWithPhoto({url:jsonNASA.data.hdurl}, {caption: jsonNASA.data.title});
-        ctx.reply(jsonNASA.data.explanation);
-      });
-    }
-    if (jsonNASA.data.media_type == 'video') {
-      bot.command('imagem', (ctx) => {
-        ctx.reply(jsonNASA.data.url);
-        ctx.reply(jsonNASA.data.title);
-        ctx.reply(jsonNASA.data.explanation);
-      });
-    };
 
+    bot.command('random',async (ctx)=> {
+      let jsonRandom;
+      try {
+        jsonRandom = await getJSONAPOD(URL_RANDOM);
+      } catch (e) {
+        jsonRandom = null;
+      };
+      if (jsonRandom) {
+        if (jsonRandom.data[0].media_type == 'image') {
+          await sendImageURL(ctx, jsonRandom);
+          await sendImageExplanation(ctx, jsonRandom);
+        }else if (jsonRandom.data[0].media_type == 'video') {
+          await sendVideoURL(ctx, jsonRandom);
+          await sendImageExplanation(ctx, jsonRandom);
+        }
+      } else {
+        await ctx.reply('Falha ao recuperar a imagem do dia. :(');
+      };
+    });
     bot.hears('oi', (ctx) => ctx.reply('Olá. Use /help para mais informações.'));
     bot.launch();
-    process.once('SIGINT', () => bot.stop('SIGINT'))
-    process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    process.once('SIGINT', () => bot.stop('SIGINT'));
+    process.once('SIGTERM', () => bot.stop('SIGTERM'));
 })();
