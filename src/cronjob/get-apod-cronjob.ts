c2ec71b163e09f65acc0259c9a9c94ad1d19f164
murilo <murilovidal@gmail.com>
2021-06-13 16:33:51 -0300

    cronjob to get apod

---
+++ src/cronjob/get-apod-cronjob.ts
@@ -0,0 +1,27 @@
+import { CronJob } from "cron";
+import { ApodDatasource } from "../data/datasource/apod.datasource";
+
+export class GetApodCronjob {
+  private cronJob: CronJob;
+  private apodDatasource: ApodDatasource;
+
+  constructor() {
+    this.apodDatasource = new ApodDatasource();
+    this.cronJob = new CronJob("0 6-10 * * *", async () => {
+      try {
+        await this.getNewApod();
+      } catch (e) {
+        console.error(e);
+      }
+    });
+    // Start job
+    if (!this.cronJob.running) {
+      this.cronJob.start();
+      console.log("Cronjob Started");
+    }
+  }
+  private async getNewApod() {
+    const apod = await this.apodDatasource.getApodFromAPI();
+    this.apodDatasource.setApod(apod);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import { getConnection } from "typeorm";
 import { BotService } from "./service/bot.service";
 import { EnvService } from "./service/env-service";
 import { TelegramPresentation } from "./web/telegram-presentation";
-import dotenv from "dotenv";
 import { DbConnectionHelper } from "./helper/db-connection-helper";
-dotenv.config();
+import { GetApodCronjob } from "./cronjob/get-apod-cronjob";
 
 const envService = new EnvService();
 const telegramPresentation = new TelegramPresentation(
@@ -13,9 +12,12 @@ const telegramPresentation = new TelegramPresentation(
 const dbConnectionHelper = new DbConnectionHelper();
 
 (async () => {
+  console.log(process.env);
+
   await dbConnectionHelper.makeConnection();
   const connection = getConnection();
   await connection.synchronize();
   telegramPresentation.startBot();
   console.log("Bot started");
+  const getApodCronjob = new GetApodCronjob();
 })();
